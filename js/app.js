be98097e8949a84083f0be168035875839c6ea76
Eslam Akram <eslam_akram91@yahoo.com>
2021-07-26 22:37:02 +0300

    changed app.js

--- js/app.js
+++ js/app.js
@@ -7,10 +7,10 @@ let q4= prompt("Is the Amazon rain forest produces half the world’s oxygen sup
 let q5= prompt("Is Cabir.A the first cell phone virus?y/n").toLocaleLowerCase();//yes it is in 2004
 
 
-let questions =[q1,q2,q3,q4,q5];
+//let questions =[q1,q2,q3,q4,q5];
 
-for (i=0;i<5;i++){
-switch(questions(i)) {
+//for (i=0;i<5;i++){
+switch(q1) {
     case 'y':
        console.log('yes, your answer is right!');
       alert("You are right!");
@@ -21,7 +21,22 @@ switch(questions(i)) {
       console.log('you have a problem');
       break;
     }// end of switch 
-}//end of for loop
+//}//end of for loop
+
+if (q2 = 'y') {
+    console.log('yes, your answer is right!');
+      alert("yes it is grow nearly 4 times than toenails!");
+} else if (q3 = 'y') {
+    console.log('yes, your answer is right!');
+      alert("yes, your answer is right!");
+} else if (q4 = 'y') {
+    console.log('yes, your answer is right!');
+      alert("yes, your answer is right!");
+} else if (q5 = 'y') {
+    console.log('yes, your answer is right!');
+      alert("yes, your answer is right!");
+    
+}
 
 let user = prompt("Kindly what is your name?").toLocaleUpperCase();
-alert("You are welcome" + user + 'Nice to meet you. I hope to see you again here!');
+alert("You are welcome " + user + ' Nice to meet you. I hope to see you again here!');
