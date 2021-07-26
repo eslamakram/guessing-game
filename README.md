62efa83b089869cae5b4dec53794cee3aeb9bdab
Eslam Akram <eslam_akram91@yahoo.com>
2021-07-26 21:57:03 +0300

    finishing my project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# guessing-game
+# About Me guessing-game
+
+I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+
+  *Author: Eslam Tweikat*
+
+**Links and Resources**
+
+**Reflections and Comments**
--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 'use strict';
+let userInformation = ['Eslam', 'Jordan', 'Two', 'apple','Turkey'];
+
+let q1= prompt("Do you search about Eslam?y/n").toLocaleLowerCase();
+let q2= prompt("Do you know that Eslam lives in Jordan?y/n").toLocaleLowerCase();
+let q3= prompt("Do you Know that Eslam has Two Girls?y/n").toLocaleLowerCase();
+let q4= prompt("Do you know that Eslam loves apple?y/n").toLocaleLowerCase();
+let q5= prompt("Do you know that places she would like to visit it?y/n").toLocaleLowerCase();
+
+
+let questions =[q1,q2,q3,q4,q5];
+
+for (i=0;i<= questions.length;i++){
+switch(questions(i)) {
+    case 'y':
+       console.log('yes, your answer is right!');
+      alert("You are right!");
+      break;
+    case 'n':  
+      break;
+    default:
+      console.log('you have a problem');
+      break;
+    }// end of switch 
+}//end of for loop
+
+let user = prompt("Kindly what is your name?").toLocaleUpperCase();
+alert("You are welcome" + user + 'Nice to meet you. I hope to see you again here!');
