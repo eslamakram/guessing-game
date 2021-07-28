92603358272e7b8a864c968bb805dea2442ed774
Eslam Akram <eslam_akram91@yahoo.com>
2021-07-28 18:06:56 +0300

    finishing lab04

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # About Me guessing-game
 
-I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+I built an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
 
 Author: Eslam Tweikat
 
@@ -15,3 +15,14 @@ we added a new features as the user's requirements :
 3. added a guessing number game with 4 attempts
 4. added a guessing favorite places game with 6 attempts
 5. display user's final score
+
+28 JULY 2021
+We work in pair programming so the names of the Driver and Navigator of this work are that was worked together :
+
+1. Driver is Raneem Abu Jamous
+2. Navigator is Eslam Tweikat  
+
+we refactored the code with function to be Clean and DRY:
+
+1. added function for each question
+2. minimize the code by using array (question, correctAnswer,responseAnswer)
--- js/app.js
+++ js/app.js
@@ -1,88 +1,46 @@
 'use strict';
 
+// declerations
+
 let finalScore = 0;
+let questions = ["Did you know that Our eyes can detect sound?y/n", "Are Our eyes the same size from birth?y/n", "Is Fingernails grow faster than toenails?y/n" ,
+"Is the Amazon rain forest produces half the world’s oxygen supply?y/n", "Is Cabir.A the first cell phone virus?y/n" ];
+let responseAnswers = ['yes', 'y', 'no', 'n'];
+let correctAnswers = ['yes, your answer is right!', 'yes, correct answer!', 'yes, correct answer. it is grow nearly 4 times than toenails!' ,"yes, your answer is right!"
+, 'yes, your answer is right! it is in 2004'];
+let ans = [];
 
-// first question 
+// functions
 
-let q1= prompt("Did you know that Our eyes can detect sound?y/n").toLocaleLowerCase();
+// 1-5 questions 
 
-while(q1 !== 'yes' && q1 !== 'y' && q1 !== 'no' && q1 !== 'n') {
-  q1= prompt("Did you know that Our eyes can detect sound?y/n").toLocaleLowerCase();
-}
-if(q1 === 'yes' || q1 === 'y') {
-  finalScore++;
-  alert('yes, your answer is right!');
-}
-else{
-  console.log('no, your answer is incorrect!'); 
+function askUser(questions, responseAnswers, correctAnswers) {
+  for (let i=0 ; i < questions.length ; i++){
+     ans[i] = prompt (questions[i]).toLocaleLowerCase();
 
-}
-console.log(`finalScore ${finalScore}`);
- 
-// second question 
+    while(ans[i] !== responseAnswers[0] && ans[i] !== responseAnswers[1] && ans[i] !== responseAnswers[2] && ans[i] !== responseAnswers[3]) {
+        ans[i] = prompt (questions[i]).toLocaleLowerCase();
+    }// end while
 
-let q2= prompt("Are Our eyes the same size from birth?y/n").toLocaleLowerCase();//yes the same
-// should the user enter yes or no 
-while(q2 !== 'yes' && q2 !== 'y' && q2 !== 'no' && q2 !== 'n') {
-  q2= prompt("Are Our eyes the same size from birth?y/n").toLocaleLowerCase();
-}
-if(q2 === 'yes' || q2 === 'y') {
-  finalScore++;
-  alert('yes, correct answer!');
-}
-else{
-  console.log('no, your answer is incorrect!'); 
+    if(ans[i] === responseAnswers[0] || ans[i] === responseAnswers[1]) {
+      finalScore++;
+      alert(correctAnswers[i]);
+    }
+    else{
+      alert('no, your answer is incorrect!'); 
+    
+    }
+    console.log(`finalScore ${finalScore}`);
 
-}
-console.log(`finalScore ${finalScore}`);
+  } // end of for loop 
 
-// third question 
+}// end of askUser function
 
-let q3= prompt("Is Fingernails grow faster than toenails?y/n").toLocaleLowerCase();//yes it is grow nearly 4 times than toenails
-while(q3 !== 'yes' && q3 !== 'y' && q3 !== 'no' && q3 !== 'n') {
-  q3= prompt("Is Fingernails grow faster than toenails?y/n").toLocaleLowerCase();//yes it is grow nearly 4 times than toenails
-}
-if(q3 === 'yes' || q3 === 'y') {
-  finalScore++;
-  alert('yes, correct answer. it is grow nearly 4 times than toenails!');
-}
-else{
-  console.log('no, your answer is incorrect!'); 
 
-}
-console.log(`final Score is ${finalScore}`);
+function guessingNumberGame(){
 
-// fourth question 
-let q4= prompt("Is the Amazon rain forest produces half the world’s oxygen supply?y/n").toLocaleLowerCase();//yes
-while (q4 !== 'yes' && q4 !=='y' && q4 !== 'no' && q4 !== 'n'){
-  q4= prompt("Is the Amazon rain forest produces half the world’s oxygen supply?y/n").toLocaleLowerCase()
-}
-if (q4 == 'y' || q4 == 'yes') {
-  finalScore++;
-    console.log('yes, your answer is right!');
-      alert("yes, your answer is right!");
-} else{
-  console.log('no,your answer is incorrect!');
-}
-console.log(`final score is ${finalScore}`);
-
-// fifth question 
-let q5= prompt("Is Cabir.A the first cell phone virus?y/n").toLocaleLowerCase();//yes it is in 2004
-while (q5 !== 'yes' && q5 !=='y' && q5 !== 'no' && q5 !== 'n'){
-  q5= prompt("Is Cabir.A the first cell phone virus?y/n").toLocaleLowerCase();//yes it is in 2004
-}
-if (q5 == 'y' || q5 == 'yes') {
-  finalScore++;
-    console.log('yes, your answer is right!');
-      alert("yes, your answer is right!");
-} else{
-  console.log('no,your answer is incorrect!');
-}
-console.log(`final score is ${finalScore}`);
-
-// 6th question 
-let holdingNumber= Math.floor((Math.random() * 100) + 1);
-let guessingNumber= prompt("let's have fun! i have a number in my mind,Could you guess it?");
+  let holdingNumber= Math.floor((Math.random() * 100) + 1);
+let guessingNumber= Number(prompt("let's have fun! i have a number in my mind,Could you guess it?"));
 
 let attempts;
 for (attempts=1; attempts < 4; attempts++){
@@ -96,14 +54,13 @@ for (attempts=1; attempts < 4; attempts++){
         guessingNumber= prompt("let's have fun! i have a number in my mind,Could you guess it?");
       } else {
         alert("It's too low!");
-        guessingNumber= prompt("let's have fun! i have a number in my mind,Could you guess it?");
-
-      }
+        guessingNumber= prompt("let's have fun! i have a number in my mind,Could you guess it?");      }
 } //end for loop for 4 attempts
 console.log(`the final score is ${finalScore}`) 
+} // end of guessingNumberGame()
 
-// 7th question
-let myFavPlaces =['turkey','spain','palestine'];
+function guessingPlaceGame(){
+  let myFavPlaces =['turkey','spain','palestine'];
 let guessingPlace= prompt("Having fun! Could you guess one of my favorite place that want to visit?");
 
 let opportunities;
@@ -115,15 +72,31 @@ outerLoop: for (opportunities = 1; opportunities < 6; opportunities++){
     console.log('yes, your answer is right!');
       alert(`yes, your answer is right! ${myFavPlaces[i]}`);
     break outerLoop;}
-      
-  } // end of array loop
+        } // end of array loop
  
     guessingPlace= prompt("Having fun! Could you guess one of my favorite place that want to visit?");
+   }//end for loop for 6 opportunities
+}// end of guessingPlaceGame()
+
+function informUser(){
+  let user = prompt("Kindly what is your name?");
+  user = user.toLocaleUpperCase();
+alert(`You are welcome   ${user}  Nice to meet you. I hope to see you again here! your score is ${finalScore}`);
+
+}  //end of informUser()
+
+// main code 
+
+askUser(questions,responseAnswers,correctAnswers);
+guessingNumberGame();
+guessingPlaceGame();
+informUser();
+
+
+
+
+
   
 
-  }//end for loop for 6 opportunities
 
 
-let user = prompt("Kindly what is your name?");
-user = user.toLocaleUpperCase();
-alert(`You are welcome   ${user}  Nice to meet you. I hope to see you again here! your score is ${finalScore}`);
